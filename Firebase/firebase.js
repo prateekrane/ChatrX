a0ff8f14d6735c05d85868a4eedc489ffb2a0636
Prateek Rane <99045779+prateekrane@users.noreply.github.com>
2025-05-09 16:09:23 +0530

    Update firebase.js

--- Firebase/firebase.js
+++ Firebase/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBmyk1CJ0Z1oTsxXL_kA67KHjz3F3AoSBc",
-    authDomain: "roomc-83293.firebaseapp.com",
-    projectId: "roomc-83293",
-    storageBucket: "roomc-83293.appspot.com", // Modified this line
-    messagingSenderId: "756629527166",
-    appId: "1:756629527166:web:25f03678f866e6a593162f"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "", // Modified this line
+    messagingSenderId: "",
+    appId: ""
 };
 
 // Initialize Firebase
