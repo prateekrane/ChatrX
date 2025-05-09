002ab3f11d9c143f507ad535b4c2a15f6244d07f
Prateek Rane <99045779+prateekrane@users.noreply.github.com>
2025-05-09 16:10:09 +0530

    Update firebaseHelper.js

--- utils/firebaseHelper.js
+++ utils/firebaseHelper.js
@@ -4,12 +4,7 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 const firebaseConfig = {
     // ...your firebase configuration...
-    apiKey: "AIzaSyBmyk1CJ0Z1oTsxXL_kA67KHjz3F3AoSBc",
-    authDomain: "roomc-83293.firebaseapp.com",
-    projectId: "roomc-83293",
-    storageBucket: "roomc-83293.appspot.com", // Modified this line
-    messagingSenderId: "756629527166",
-    appId: "1:756629527166:web:25f03678f866e6a593162f"
+
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
